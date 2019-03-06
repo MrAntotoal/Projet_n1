6701e8522115811bb16e21dee94b02f3d6c9214e
MrAntotoal <MrAntotoal26@gmail.com>
2019-03-06 22:51:06 +0100

    module collision test ok mais attention on sais jamais a tester en  réel

--- jeux/collisions.c
+++ jeux/collisions.c
@@ -19,6 +19,28 @@ int point_dans_polygone(Points p,polygone poly){
   double deg=0;
 
   deg=point_dans_poly_bis(p,liste_sans_premier(poly),renvoie_sommet_liste(poly),renvoie_sommet_liste(poly),0);
-  printf("deg %f\n",deg);
+  //printf("deg %f\n",deg);
   return deg==360.0;
 }
+
+int polygone_dans_polygone_bis(polygone poly0,polygone poly1){
+  if(est_list_vide(poly0)){
+    return 0;
+  }
+  else{
+    //afficher_point(renvoie_sommet_liste(poly0));
+    if(point_dans_polygone(renvoie_sommet_liste(poly0),poly1)){
+      return 1;
+    }
+    return polygone_dans_polygone_bis(liste_sans_premier(poly0),poly1);
+  }
+}
+
+int polygone_dans_polygone(polygone poly0,polygone poly1){
+  if(polygone_dans_polygone_bis( poly0, poly1)){
+    return 1;
+  }
+  return polygone_dans_polygone_bis( poly1, poly0);
+}
+
+
--- jeux/collisions.h
+++ jeux/collisions.h
@@ -2,3 +2,4 @@
 
 
 int point_dans_polygone(Points p,polygone poly);
+int polygone_dans_polygone(polygone poly0,polygone poly1);
