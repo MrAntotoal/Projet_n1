822ab0dd41e84d31c42ff14f72c8d343e83fed62
MrAntotoal <MrAntotoal26@gmail.com>
2019-03-07 16:06:40 +0100

    les types de struct pour la file de message

---
+++ jeux/type_fm.h
@@ -0,0 +1,13 @@
+#define FICHIER_CLE "Makefile"
+
+typedef struct{
+  int numero_char;
+  int type_action;
+  int a_repeter;
+}requete_t;
+
+typedef struct{
+  int numero_char;
+  int type;
+}reponse_t;
+
